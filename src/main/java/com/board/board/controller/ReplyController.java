b37e19dd660fe1f2f3e99a6bc7dac6b278e19e9f
JinYunJae <jinyun3075@naver.com>
2021-09-08 17:00:29 +0900

    모달창, Ajax 댓글 등록,수정,삭제

--- src/main/java/com/board/board/controller/ReplyController.java
+++ src/main/java/com/board/board/controller/ReplyController.java
@@ -12,16 +12,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController // 메서드의 리턴타입을 기본으로 JSON 사용
-@RequestMapping("/replies/")
+@RequestMapping("/replies")
 @Log4j2
 @RequiredArgsConstructor
 public class ReplyController {
     private final ReplyService replyService;
+
+    @PostMapping("/")
+    public ResponseEntity<Long> register(@RequestBody ReplyDto replyDto){
+        log.info(replyDto+"저장");
+        Long rno = replyService.register(replyDto);
+        return new ResponseEntity<>(rno,HttpStatus.OK);
+    }
 
     @GetMapping(value = "/board/{bno}",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ReplyDto>> getListByBoard(@PathVariable("bno") Long bno){
         log.info("bno : "+bno);
 
         return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+        log.info("댓글삭제 RNO:"+rno);
+        replyService.remove(rno);
+        return new ResponseEntity<>("success",HttpStatus.OK);
+    }
+
+    @PutMapping("/{rno}")
+    public ResponseEntity<String> modify(@RequestBody ReplyDto replyDto){
+        log.info("수정 :"+replyDto);
+
+        replyService.modify(replyDto);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 }
